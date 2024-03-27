b83252f64fcfed36e46b657d599dabe1b771ad28
kaoutar chankhar <cr.kaoutar@gmail.com>
2024-03-27 23:05:45 +0100

    commit

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update &&
 sudo apt-get install python3-pip &&
+sudo pip install -r requirements
 sudo apt-get install wslu &&
 cp $(wslpath "$(wslvar USERPROFILE)")/.kaggle/kaggle.json $HOME/.kaggle &&
 (python3 back_app.py & streamlit run app.py)
